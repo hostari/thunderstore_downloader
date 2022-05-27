bfba58affe9ae6b061463c79b136d0e509339dc6
Pauline De Polonia <paudepolonia000@yahoo.com>
2022-05-27 16:32:55 +0800

    Add download_package variable
    Refactor download algorithm
    
    Test download
    
    Test download
    
    Test download
    
    Make download algorithm work
    
    Update body
    
    Unzip folder
    
    Update process variable
    
    Add unzip process output
    
    Add conditional to unzip package
    
    Remove conditional
    
    Place download method in module
    
    Rename package_downloader to download, update instance variable syntax
    
    Create download_and_unzip method
    
    Rename to download_and_unzip_package
    
    Add get_file_hash_method
    
    Include ThunderstoreDownloader::Package module
    
    Create module for hash_generator and path_builder
    
    Separate download and unzip into two lines
    
    Wait for download to complete before unzip
    
    Refactor download_and_unzip method
    
    Refactor wqunzip method
    
    Refactor unzip method
    
    remove output in methods
    
    Refactor download method
    
    Remove output.gets_to_end
    
    Wrap variables in quotation marks
    
    Remove from Process
    
    Refactor download code
    
    Revert wrapping in Process
    
    Add user input
    
    Refactor download method again
    
    Refactor download method again
    
    Add quotation marks
    
    Refactor download method
    
    Add download_path to new ThunderstoreVersion::Package instance
    
    Update download_path type

--- src/thunderstore_downloader/get_thunderstore_file_hash.cr
+++ src/thunderstore_downloader/get_thunderstore_file_hash.cr
@@ -3,25 +3,10 @@
 module ThunderstoreDownloader
   class Package
     def self.get_thunderstore_file_hash(host : String, download_url : String, name : String, version_number : String, download_path : String)
-      package_name = `#{name}-#{version_number}`
-      zipped_package = `#{package_name}.zip`
-      `wget -O #{package_name} #{download_url}`
-
-      # package_name = `#{name}-#{version_number}`
-      # zipped_package = `#{package_name}.zip`
-
-      # download_input = `#{package_name} #{download_url}`
-      # process = Process.new("wget -O", [download_input], output: Process::Redirect::Pipe)
-      # download_package = process.output.gets_to_end
-
-      # performs unzipping
-      # unzip_input = "#{zipped_package} -d #{package_name}"
-      # process = Process.new("unzip", [unzip_input], output: Process::Redirect::Pipe)
-      # unzipped_folder = process.output.gets_to_end
-      # download_package = "#{download_path}/#{unzipped_folder}"
+      download_and_unzip_package = ThunderstoreDownloader::Package.new(name, version_number, download_url).download_and_unzip
 
       client = ThunderstoreDownloader::Client.new(host)
-      response = client.post("/api/v1/thunderstore_file_hash", download_package)
+      response = client.post("/api/v1/thunderstore_file_hash", download_and_unzip_package)
 
       if response.status_code == 200
         # PackageDownloader.new(name, version_number, download_url, download_path).download
---
+++ src/thunderstore_downloader/hash_generator.cr
@@ -0,0 +1,12 @@
+# HashGenerator returns a string the file's SHA256 hash.
+module ThunderstoreDownloader
+  class HashGenerator
+    def initialize
+    end
+
+    # returns the SHA256 hash of a given file (file path).
+    def generate_hash(path : String)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Algorithm::SHA256, "hostari", File.read(path))
+    end
+  end
+end

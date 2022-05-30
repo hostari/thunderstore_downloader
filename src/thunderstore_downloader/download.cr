b0f0b2c9de1e3dfc5e7de3a08a28ed358b7942ed
Pauline De Polonia <paudepolonia000@yahoo.com>
2022-05-30 13:58:39 +0800

    remove output in methods

--- src/thunderstore_downloader/download.cr
+++ src/thunderstore_downloader/download.cr
@@ -13,13 +13,14 @@ module ThunderstoreDownloader
     end
 
     def download
-      download_process = Process.new(`wget -O #{@name}-#{@version_number}.zip #{@download_url}`, output: Process::Redirect::Pipe)
+      #download_process = Process.new(`wget -O #{@name}-#{@version_number}.zip #{@download_url}`, output: Process::Redirect::Pipe)
+      download_process = Process.new(`wget -O #{@name}-#{@version_number}.zip #{@download_url}`)
       pp download_process.output.gets_to_end
       download_process.output.gets_to_end
     end
 
     def unzip
-      unzip_process = Process.new(`unzip #{@name}-#{@version_number}.zip -d #{@name}-#{@version_number}`, output: Process::Redirect::Pipe)
+      unzip_process = Process.new(`unzip #{@name}-#{@version_number}.zip -d #{@name}-#{@version_number}`)
 
       unzip_process.output.gets_to_end
     end

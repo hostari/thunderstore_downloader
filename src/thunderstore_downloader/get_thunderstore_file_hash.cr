ceac85d89226ee0f68672d3fbda09f38c4483f29
Pauline De Polonia <paudepolonia000@yahoo.com>
2022-05-30 11:37:25 +0800

    Rename to download_and_unzip_package

--- src/thunderstore_downloader/get_thunderstore_file_hash.cr
+++ src/thunderstore_downloader/get_thunderstore_file_hash.cr
@@ -21,10 +21,10 @@ module ThunderstoreDownloader
       # unzip_process = Process.new(`unzip #{name}-#{version_number}.zip -d #{name}-#{version_number}`, output: Process::Redirect::Pipe)
       # unzip_folder = unzip_process.output.gets_to_end
 
-      download_package = ThunderstoreDownloader::Package.new(name, version_number, download_url).download_and_unzip
+      download_and_unzip_package = ThunderstoreDownloader::Package.new(name, version_number, download_url).download_and_unzip
 
       client = ThunderstoreDownloader::Client.new(host)
-      response = client.post("/api/v1/thunderstore_file_hash", package)
+      response = client.post("/api/v1/thunderstore_file_hash", download_and_unzip_package)
 
       if response.status_code == 200
         # PackageDownloader.new(name, version_number, download_url, download_path).download

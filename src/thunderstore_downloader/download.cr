31bb56aae74e08ff742a320633de6259b92f2854
paula4230 <paudepolonia000@yahoo.com>
2022-06-01 00:50:41 +0800

    Add get hash method
    Remove extra slash

--- src/thunderstore_downloader/download.cr
+++ src/thunderstore_downloader/download.cr
@@ -8,9 +8,10 @@ module ThunderstoreDownloader
     def initialize(@name, @version_number, @download_url, @download_path)
     end
 
-    def download_and_unzip
+    def download_unzip_get_hash
       download
       unzip
+      get_thunderstore_file_hash
     end
 
     def download
@@ -21,8 +22,8 @@ module ThunderstoreDownloader
       `unzip "#{@name}-#{@version_number}.zip" -d "#{@name}-#{@version_number}"`
     end
 
-    def get_thunderstore_file_hash(download_path : String)
-      FileHash.new("#{@download_path}/#{@name}-#{@version_number}").get_file_hash
+    def get_thunderstore_file_hash
+      FileHash.new("#{@download_path}#{@name}-#{@version_number}").get_file_hash
     end
   end
 end

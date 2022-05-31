8192efcb325ea5e77ad399f1ab9a00a92404958f
paula4230 <paudepolonia000@yahoo.com>
2022-06-01 00:01:18 +0800

    Remove output.gets_to_end

--- src/thunderstore_downloader/download.cr
+++ src/thunderstore_downloader/download.cr
@@ -14,13 +14,10 @@ module ThunderstoreDownloader
 
     def download
       download_process = Process.new(`wget -O #{@name}-#{@version_number}.zip #{@download_url}`, output: Process::Redirect::Pipe)
-      download_process.output.gets_to_end
     end
 
     def unzip
       unzip_process = Process.new(`unzip #{@name}-#{@version_number}.zip -d #{@name}-#{@version_number}`)
-
-      unzip_process.output.gets_to_end
     end
 
     def get_thunderstore_file_hash(download_path : String)

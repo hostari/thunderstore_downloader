e0e4d1cbb03321b62a87efdebc60dbed3207c932
paula4230 <paudepolonia000@yahoo.com>
2022-06-07 14:06:19 +0800

    Add zipped and unzipped directory paths

--- src/thunderstore_downloader/get_thunderstore_file_hash.cr
+++ src/thunderstore_downloader/get_thunderstore_file_hash.cr
@@ -3,15 +3,18 @@ module ThunderstorePackageDownloader
     property name : String
     property version_number : String
     property download_url : String
-    property download_path : String
+    property zipped_dir_path : String
+    property unzipped_dir_path : String
 
-    def initialize(@name, @version_number, @download_url, @download_path)
+    # zipped_dir_path is the path where the zipped folder will be saved and will be utilized by wget command
+    # unzipped_dir_path is the path there the unzipped folder will be stored and will be utilized by the unzip command and get_file_hash method
+    def initialize(@name, @version_number, @download_url, @zipped_dir_path, @unzipped_dir_path)
     end
 
     def get_thunderstore_file_hash
-      `wget -O "#{@download_path}#{@name}-#{@version_number}.zip" "#{@download_url}"`
-      `unzip "#{@name}-#{@version_number}.zip" -d "#{@name}-#{@version_number}"`
-      FileHash.new("#{@download_path}#{@name}-#{@version_number}").get_file_hash
+      `wget -O "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" "#{@download_url}"`
+      `unzip "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" -d "#{@unzipped_dir_path}#{@name}-#{@version_number}"`
+      FileHash.new("#{@unzipped_dir_path}-#{@version_number}").get_file_hash
     end
   end
 end

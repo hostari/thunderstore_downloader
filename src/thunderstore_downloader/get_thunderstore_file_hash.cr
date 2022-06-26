6fa7b1a8633fd730c2fc8c5146a20d20ba65540b
paula4230 <paudepolonia000@yahoo.com>
2022-06-26 23:12:32 +0800

    Prevent redownloading folder

--- src/thunderstore_downloader/get_thunderstore_file_hash.cr
+++ src/thunderstore_downloader/get_thunderstore_file_hash.cr
@@ -12,7 +12,7 @@ module ThunderstorePackageDownloader
     end
 
     def get_thunderstore_file_hash
-      `wget -O "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" "#{@download_url}"`
+      `wget -c "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" "#{@download_url}"`
       `unzip -o "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" -d "#{@unzipped_dir_path}#{@name}-#{@version_number}"`
       FileHash.new("#{@unzipped_dir_path}#{@name}-#{@version_number}").get_file_hash
     end

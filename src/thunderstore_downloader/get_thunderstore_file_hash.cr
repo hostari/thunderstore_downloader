5c11a77caa421503c33b3f2ce934d99f407966ec
paula4230 <paudepolonia000@yahoo.com>
2022-10-13 14:42:12 +0800

    Refactor methods

--- src/thunderstore_downloader/get_thunderstore_file_hash.cr
+++ src/thunderstore_downloader/get_thunderstore_file_hash.cr
@@ -11,10 +11,45 @@ module ThunderstorePackageDownloader
     def initialize(@name, @version_number, @download_url, @zipped_dir_path, @unzipped_dir_path)
     end
 
+    # def get_thunderstore_file_hash
+    #   `wget -O "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" "#{@download_url}"`
+    #   `unzip -o "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" -d "#{@unzipped_dir_path}#{@name}-#{@version_number}"`
+    #   FileHash.new("#{@unzipped_dir_path}#{@name}-#{@version_number}").get_file_hash
+    # end
+
     def get_thunderstore_file_hash
-      `wget -O "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" "#{@download_url}"`
-      `unzip -o "#{@zipped_dir_path}#{@name}-#{@version_number}.zip" -d "#{@unzipped_dir_path}#{@name}-#{@version_number}"`
-      FileHash.new("#{@unzipped_dir_path}#{@name}-#{@version_number}").get_file_hash
+      download_command
+      unzip_command
+      get_file_hash_command
+    end
+
+    def delete_files
+      `sudo rm -rf #{mod_version_unzipped}`
+      `sudo rm -rf #{mod_version_zipped}`
+    end
+
+    def download_command
+      `wget -O "#{mod_version_zipped}" "#{@download_url}"`
+    end
+
+    def unzip_command
+      `unzip -o "#{mod_version_zipped}" -d "#{mod_version_unzipped}"`
+    end
+
+    def get_file_hash_command
+      FileHash.new("#{mod_version_unzipped}").get_file_hash
+    end
+
+    def mod_version
+      "#{@name}-#{@version_number}"
+    end
+
+    def mod_version_unzipped
+      "#{@unzipped_dir_path}#{mod_version}"
+    end
+
+    def mod_version_zipped
+      "#{@zipped_dir_path}#{mod_version}.zip"
     end
   end
 end
